3d560ff26c473817a4dfb33370a36f40d5776283
Anas Abudayah <anas.abudayah@gmail.com>
2014-10-01 16:50:25 +0300

    general fixes

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -11,6 +11,11 @@ $this->pageTitle=Yii::app()->name;
 	<?php echo Yii::app()->user->email; ?><br>
 	<?php echo Yii::app()->user->birth; ?><br>
 	Is Admin: <?php echo var_dump(Yii::app()->user->checkAccess('admin')); ?><br>
+	<hr>
 <?php } ?>
 
-<?php echo GZero::langSwitcher(); ?>
+<h4>Useful links</h4>
+<ul>
+	<li><a href="http://bootsnipp.com/" target="_blank" >Bootsnipp</a> Design elements, playground and code snippets for <a href="http://getbootstrap.com" target="_blank">Bootstrap</a> HTML/CSS/JS framework</li>
+	<li><a href="http://yiiwheels.2amigos.us/" target="_blank" >YiiWheels</a> Wheels is an extension library for the <a href="http://yiiwheels.2amigos.us/" target="_blank" >YiiStrap</a> extension.</li>
+</ul>
